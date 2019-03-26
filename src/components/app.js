851e33470eb588986da429d25cc99c295276b6e9
andori <falcsmike7@gmail.com>
2019-03-25 20:04:44 -0400

    used await async to pull data from unsplash api

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  state = { images: [] }
+
+  onSearchSubmit = async term => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID 17ceb2a2592b390b6cbc6102dba4a140155cdc93d9b08a8f56d06cc3e2a7371d'
+      }
+    })
+
+    this.setState({ images: response.data.results });
   }
 
   render() {
